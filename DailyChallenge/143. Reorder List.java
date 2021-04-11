bca10d6a2221c9dcb4b2cc9ac676a515a920d9b2
Ian <44253879+ian2a@users.noreply.github.com>
2021-04-11 00:16:37 -0400

    Create 143. Reorder List.java

---
+++ DailyChallenge/143. Reorder List.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        ListNode mid = findMid(head);
+        ListNode newMid = reverse(mid.next);
+        mid.next = null;
+        merge(head, newMid);
+    }
+    
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = head;
+        ListNode prev = null;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    
+    private void merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            cur.next = l1;
+            l1 = l1.next;
+            cur.next.next = l2;
+            l2 = l2.next;
+            cur = cur.next.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        l1 = dummy.next;
+    }
+}
