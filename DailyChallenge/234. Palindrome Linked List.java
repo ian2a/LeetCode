9487aac39506a91d6d9fbeba66dace0eeaad9afc
Ian <44253879+ian2a@users.noreply.github.com>
2021-04-13 22:08:00 -0400

    Create 234. Palindrome Linked List.java

---
+++ DailyChallenge/234. Palindrome Linked List.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head.next == null) {
+            return true;
+        }
+        ListNode firstTail = getMid(head);
+        ListNode secHead = firstTail.next;
+        firstTail.next = null;
+        secHead = reverse(secHead);
+        
+        return compare(head, secHead);
+    }
+    
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    
+    private boolean compare(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            if (l1.val != l2.val) {
+                return false;
+            } else {
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+        }
+        
+        return true;
+    }
+}
