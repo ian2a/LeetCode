30b8b058175b908353fe1992ae5529ddbedbd361
Ian <44253879+ian2a@users.noreply.github.com>
2021-04-25 01:01:51 -0400

    Create 264. Ugly Number II.java

---
+++ DailyChallenge/264. Ugly Number II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
+        for (int i = 1; i < n; i++) {
+            res[i] = Math.min(Math.min(res[p2] * 2, res[p3] * 3), res[p5] * 5);
+            if (res[i] == res[p2] * 2) {
+                p2++;
+            }
+            if (res[i] == res[p3] * 3) {
+                p3++;
+            }
+            if (res[i] == res[p5] * 5) {
+                p5++;
+            }
+        }
+        return res[n - 1];
+    }
+}
